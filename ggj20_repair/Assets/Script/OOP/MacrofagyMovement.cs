8782ef68c7ca257533e8b6ee297e58f847c35902
Simone Durante <lovecry@hotmail.com>
2020-02-02 16:44:26 +0100

    fic

--- ggj20_repair/Assets/Script/OOP/MacrofagyMovement.cs
+++ ggj20_repair/Assets/Script/OOP/MacrofagyMovement.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class MacrofagyMovement : MonoBehaviour
 {
     Vector3 DestinationPosition = Vector3.zero;
+    Vector3 Direction = Vector3.zero;
 
     // Update is called once per frame
     void Update()
     {
         if (transform.position.y > 3)
-            transform.position -= new Vector3(0, 5, 0);
+        {
+            Vector3 positionOffset = Direction - new Vector3(0, 5, 0);
+            transform.position -= positionOffset;
+        }
         else
         {
             if (DestinationPosition == Vector3.zero)
@@ -28,7 +32,7 @@ public class MacrofagyMovement : MonoBehaviour
 
     public void SetDirection(Vector3 direction)
     {
-
+        Direction = direction;
     }
 
     void OnCollisionEnter(Collision col)
