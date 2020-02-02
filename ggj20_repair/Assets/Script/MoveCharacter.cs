f009095da39bf115597f05558d7691b9789f6638
CesareMontresor <cesare.montresor@gmail.com>
2020-02-02 15:22:27 +0100

    aggiunti menu & moviemento navicella

--- ggj20_repair/Assets/Script/MoveCharacter.cs
+++ ggj20_repair/Assets/Script/MoveCharacter.cs
@@ -18,13 +18,30 @@ public class MoveCharacter : MonoBehaviour
     void Update()
     {
 
-       float hAxis = Input.GetAxis("Horizontal");
+        float hAxis = Input.GetAxis("Horizontal");
         float vAxis = Input.GetAxis("Vertical");
 
         Vector3 mov3 = new Vector3(hAxis * speed, 0 , vAxis * speed);
         
-        this.transform.position += mov3 * Time.deltaTime ;
-        //transform.rotation = Quaternion.LookRotation(new Vector3(hAxis, 0, vAxis).normalized);
+        this.transform.position += mov3 * Time.deltaTime;
 
+        float v = Mathf.Round(vAxis);
+        float h = Mathf.Round(hAxis);
+
+        float angle=90;
+        if (v > 0 && h == 0) { angle += 0; } //top
+        if (v > 0 && h > 0) { angle += 45; } //top-right
+        if (v == 0 && h > 0) { angle += 90; } //right
+        if (v < 0 && h > 0) { angle += 135; }  //bot-right
+        if (v < 0 && h == 0) { angle += 180; } //bot
+        if (v < 0 && h < 0) { angle += 225; } //bot-left
+        if (v == 0 && h < 0) { angle += 270; } //bot-left
+        if (v > 0 && h < 0) { angle += 315; } //bot-left
+
+
+        if ( Mathf.Abs(vAxis) > 0 && Mathf.Abs(vAxis) > 0 )
+        {
+            transform.rotation = Quaternion.Euler(-90, 0, angle);
+        }
     }
 }
